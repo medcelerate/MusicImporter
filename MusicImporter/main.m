9652cfefc703807ce558485d26e95c4699a3f682
evan.clark <evan.clark@health.nyc.gov>
2024-09-15 11:35:00 -0400

    Initial Commit

---
+++ MusicImporter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicImporter
+//
+//  Created by Evan Clark on 9/15/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
